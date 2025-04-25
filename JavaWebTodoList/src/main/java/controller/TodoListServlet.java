56ffa5a9ae81589c314a7ef0ed0ef912ff1dde8b
NTPU <NTPU@503-tr>
2025-04-25 14:04:29 +0800

    web

--- JavaWebTodoList/src/main/java/controller/TodoListServlet.java
+++ JavaWebTodoList/src/main/java/controller/TodoListServlet.java
@@ -1,41 +1,34 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.dto.TodoDTO;
+import service.TodoListService;
+import service.TodoListServiceImpl;
 
 @WebServlet("/todolist/*")
 public class TodoListServlet extends HttpServlet  {
-
+	
+	private TodoListService service = new TodoListServiceImpl();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String pathInfo = req.getPathInfo();
 		resp.getWriter().print("pathInfo = " + pathInfo);
 		switch (pathInfo) {
 			case "/": // 顯示 Todo List 首頁
-				
-				break;
-			case "/update": // 修改 Todo 紀錄
-				String id = req.getParameter("id");
-				String completed = req.getParameter("checked");
-				String text = req.getParameter("text");
-				
-				if(completed != null) {
-					// 修改 Todo 完成狀態
-					
-				} else if (text != null) {
-					// 修改 Todo 內容
-					
-				}
-				
-				break;
-			case "/delete": // 刪除某一項 Todo 紀錄
-				
-				break;
+				List<TodoDTO> todos = service.findAllTodos();
+				RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/todolist.jsp");
+				req.setAttribute("todos", todos);
+				rd.forward(req, resp);
+				return;
 			default:  // 錯誤路徑
 				resp.getWriter().print("path error");
 				return;
