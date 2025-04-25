e7b1bd38febce0af4c1dbf95519c5d96675a016c
NTPU <NTPU@503-tr>
2025-04-25 11:54:09 +0800

    web

--- JavaWebTodoList/src/main/java/service/TodoListServiceImpl.java
+++ JavaWebTodoList/src/main/java/service/TodoListServiceImpl.java
@@ -1,5 +1,62 @@
 package service;
 
-public class TodoListServiceImpl {
+import java.util.List;
+
+import dao.TodoListDao;
+import dao.TodoListDaoImpl;
+import model.dto.TodoDTO;
+import model.entity.Todo;
+
+public class TodoListServiceImpl implements TodoListService {
+	
+	private TodoListDao dao = new TodoListDaoImpl();
+	
+	@Override
+	public List<TodoDTO> findAllTodos() {
+		List<Todo> todos = dao.findAllTodos();
+		// List<Todo> 轉 List<TodoDTO>
+		
+		return null;
+	}
+
+	@Override
+	public TodoDTO getTodo(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTodo(String text, Boolean completed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTodoComplete(Integer id, Boolean completed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTodoText(Integer id, String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteTodo(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	// 轉換成 DTO
+	private TodoDTO transferToDTO(Todo todo) {
+		return new TodoDTO(todo.getId(), todo.getText(), todo.getCompleted());
+	}
+	
+	// 轉換成 Entity
+	private Todo transferToEntity(TodoDTO todoDTO) {
+		return new Todo(todoDTO.getId(), todoDTO.getText(), todoDTO.getCompleted());
+	}
 
 }
