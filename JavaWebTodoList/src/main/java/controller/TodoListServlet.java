ce0b176bdcd56ebb1608ab4daad605d35071d487
NTPU <NTPU@503-tr>
2025-04-25 12:16:49 +0800

    web

--- JavaWebTodoList/src/main/java/controller/TodoListServlet.java
+++ JavaWebTodoList/src/main/java/controller/TodoListServlet.java
@@ -1,5 +1,21 @@
 package controller;
 
-public class TodoListServlet {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/todolist/*")
+public class TodoListServlet extends HttpServlet  {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String pathInfo = req.getPathInfo();
+		resp.getWriter().print("pathInfo = " + pathInfo);
+	}
+	
+	
 }
