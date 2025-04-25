040a1b226e7582649c5840f2b50cdb914bf59527
NTPU <NTPU@503-tr>
2025-04-25 10:21:51 +0800

    web

--- JavaWebTodoList/src/main/java/model/dto/TodoDTO.java
+++ JavaWebTodoList/src/main/java/model/dto/TodoDTO.java
@@ -1,5 +1,14 @@
 package model.dto;
 
-public class TodoDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoDTO {
+	private Integer id; // 序號
+	private String text; // 工作項目
+	private Boolean completed; // 項目是否完成?
 }
