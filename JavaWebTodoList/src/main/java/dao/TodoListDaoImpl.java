d312e4fa8501873add68bada292d374d18aa93f4
NTPU <NTPU@503-tr>
2025-04-25 11:08:13 +0800

    web

--- JavaWebTodoList/src/main/java/dao/TodoListDaoImpl.java
+++ JavaWebTodoList/src/main/java/dao/TodoListDaoImpl.java
@@ -1,5 +1,68 @@
 package dao;
 
-public class TodoListDaoImpl {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entity.Todo;
+
+public class TodoListDaoImpl extends BaseDao implements TodoListDao {
+
+	@Override
+	public List<Todo> findAllTodos() {
+		List<Todo> todos = new ArrayList<>();
+		
+		String sql = "select id, text, completed from todo order by id";
+		try(Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql)) {
+			// 逐一尋訪 rs 中的每一筆紀錄
+			// 再將每一筆紀錄轉成 Todo 物件
+			// 最後加入到 todos 集合中
+			while(rs.next()) {
+				Todo todo = new Todo();
+				todo.setId(rs.getInt("id"));
+				todo.setText(rs.getString("text"));
+				todo.setCompleted(rs.getBoolean("completed"));
+				
+				todos.add(todo);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+ 		
+		return todos;
+	}
+
+	@Override
+	public Todo getTodo(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTodo(Todo todo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTodoComplete(Integer id, Boolean completed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTodoText(Integer id, String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteTodo(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
