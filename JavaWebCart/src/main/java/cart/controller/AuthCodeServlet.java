d3faec765893a3885f77f140122635553216ed04
NTPU <NTPU@503-tr>
2025-04-28 13:58:28 +0800

    cart

---
+++ JavaWebCart/src/main/java/cart/controller/AuthCodeServlet.java
@@ -0,0 +1,49 @@
+package cart.controller;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/user/authcode")
+public class AuthCodeServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String authcode = "1234";
+		ImageIO.write(getAuthCodeImage(authcode), "JPEG", resp.getOutputStream());
+	}
+	
+	// 利用 Java2D 產生動態圖像
+	private BufferedImage getAuthCodeImage(String authcode) {
+		// 建立圖像區域(80x30 TGB)
+		BufferedImage img = new BufferedImage(80, 30, BufferedImage.TYPE_INT_RGB);
+		// 建立畫布
+		Graphics g = img.getGraphics();
+		// 設定顏色
+		g.setColor(Color.YELLOW);
+		// 塗滿背景
+		g.fillRect(0, 0, 80, 30); // 全區域
+		// 設定顏色
+		g.setColor(Color.BLACK);
+		// 設定字型
+		g.setFont(new Font("Arial", Font.BOLD, 22)); // 字體, 風格, 大小
+		// 繪文字
+		g.drawString(authcode, 18, 22); // (18, 22) 表示繪文字左上角的起點
+		
+		
+		return img;
+		
+	}
+	
+	
+}
