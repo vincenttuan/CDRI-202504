10e7bba4b623ce9804d80103ffdafcad20033e00
NTPU <NTPU@503-tr>
2025-04-29 12:08:19 +0800

    cart

---
+++ JavaWebCart/src/main/java/cart/service/ProductService.java
@@ -0,0 +1,12 @@
+package cart.service;
+
+import java.util.List;
+
+import cart.model.entity.Product;
+
+public interface ProductService {
+	List<Product> findAllProducts(); // 查詢全部
+	void add(String productName, String price, String qty, String productUmageBase64); // 新增
+	void delete(Integer productId); // 刪除, 根據指定 productId
+
+}
