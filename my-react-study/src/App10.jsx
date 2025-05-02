c3eba0fbecd50d432aa826888b93f3eb3a391e9c
NTPU <NTPU@503-tr>
2025-05-02 15:08:12 +0800

    react

---
+++ my-react-study/src/App10.jsx
@@ -0,0 +1,16 @@
+/*
+{id:1, name:'小明', score:55},
+{id:2, name:'小美', score:78},
+{id:3, name:'小華', score:92},
+{id:4, name:'阿強', score:40},
+
+顯示 table 樣式
+id | name | score | pass
+ 1   小明    55      X 
+ 2   小美    78      V 
+ 3   小華    92      V 
+ 4   阿強    40      X 
+全班平均: XX
+
+試著拆分父子組件
+*/
