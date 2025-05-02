1be67ff35b42a0d928b42e8bba12e1cb5cf0e586
NTPU <NTPU@503-tr>
2025-05-02 12:08:10 +0800

    react

--- my-react-study/src/main.jsx
+++ my-react-study/src/main.jsx
@@ -4,10 +4,11 @@ import App from './App.jsx'
 import App2 from './App2.jsx'
 import App3 from './App3.jsx'
 import App4 from './App4.jsx'
+import App5 from './App5.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App4 />
+    <App5 />
   </React.StrictMode>,
 )
