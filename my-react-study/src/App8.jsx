b6cd217af765eabbf6eb4fcec5fbca6d451c4c6e
NTPU <NTPU@503-tr>
2025-05-02 13:51:50 +0800

    react

---
+++ my-react-study/src/App8.jsx
@@ -0,0 +1,8 @@
+/*
+商品資料如下:
+{ id: 1, name: '蘋果', price: 40, category: '水果' },
+{ id: 2, name: '洗髮精', price: 120, category: '日用品' },
+{ id: 3, name: '香蕉', price: 55, category: '水果' },
+{ id: 4, name: '牙膏', price: 45, category: '日用品' }
+請利用 react 將上述商品資料透過 jsx + <table> 標籤呈現
+*/
