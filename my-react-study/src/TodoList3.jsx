3125c4c46f2d5d692225f04e2e1171b9d901046d
NTPU <NTPU@503-tr>
2025-05-05 13:42:31 +0800

    react

--- my-react-study/src/TodoList3.jsx
+++ my-react-study/src/TodoList3.jsx
@@ -1,16 +1,28 @@
 // TodoList 練習
 import './App.css'
+import { useState } from 'react';
 
 function App() {
 
-    const todos = ['吃早餐', '做體操', '寫程式'];
+    //const todos = ['吃早餐', '做體操', '寫程式'];
+    const [todos, setTodos] = useState(['吃早餐', '做體操', '寫程式'])
+    const [todo, setTodo] = useState('');
+
+    const handleTodoChange = (e) => {
+        setTodo(e.target.value);
+    }
+
+    const handleTodoAdd = () => {
+        setTodos([...todos, todo]);
+        setTodo(''); // 清空欄位資料
+    }
 
     return (
         <>
             <h1>My TodoList</h1>
             <div>
-                <input />
-                <button>加入</button>
+                <input type='text' value={todo} onChange={handleTodoChange} />
+                <button onClick={handleTodoAdd}>加入</button>
             </div>
             <ul>
                 {
