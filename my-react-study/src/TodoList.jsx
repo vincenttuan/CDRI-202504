943f9a2be954d287b168f9ac72b3a7b9a0159e52
NTPU <NTPU@503-tr>
2025-05-05 12:02:13 +0800

    react

---
+++ my-react-study/src/TodoList.jsx
@@ -0,0 +1,22 @@
+// TodoList 練習
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1>My TodoList</h1>
+            <div>
+                <input />
+                <button>加入</button>
+            </div>
+            <ul>
+                <li>吃早餐</li>
+                <li>做體操</li>
+                <li>寫程式</li>
+            </ul>
+        </>
+    )
+}
+
+export default App
+
