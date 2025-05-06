5873e17aab92bb6db25a6f6e3178394ae329694b
NTPU <NTPU@503-tr>
2025-05-06 12:07:22 +0800

    react

--- my-react-study/src/main.jsx
+++ my-react-study/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Cart3.jsx'
+import App from './Cart4.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
