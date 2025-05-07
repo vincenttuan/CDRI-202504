99a5c61bd75030e3c0e490bb48762db136079687
NTPU <NTPU@503-tr>
2025-05-07 10:21:58 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/HelloController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/HelloController.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
