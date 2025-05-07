9af221ef296fc2afd0cba0dc6d2c8a4fac65ead0
NTPU <NTPU@503-tr>
2025-05-07 11:27:06 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/HelloController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.Date;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,6 +21,13 @@ public class HelloController {
 	@ResponseBody
 	public String hi() {
 		return "Hi " + new Date();
+	}
+	
+	@GetMapping("/welcome")
+	public String welcome(Model model) {
+		model.addAttribute("name", "阿山哥");
+		model.addAttribute("now", new Date());
+		return "welcome"; // 取 welcome.jsp 檔名的部分
 	}
 	
 }
