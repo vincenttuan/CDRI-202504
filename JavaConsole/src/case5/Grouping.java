6d4ce3fe1e1af06b3a2bfd029057f84985e028c8
NTPU <NTPU@503-tr>
2025-05-07 15:55:07 +0800

    web

---
+++ JavaConsole/src/case5/Grouping.java
@@ -0,0 +1,14 @@
+package case5;
+
+import java.util.List;
+
+public class Grouping {
+
+	public static void main(String[] args) {
+		List<String> fruits = List.of("apple", "apple", "banana", "apple", "orange", "banana", "papaya");
+		// 請問每一種水果各有幾個 ?
+		
+
+	}
+
+}
