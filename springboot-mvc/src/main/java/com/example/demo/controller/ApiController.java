8bf20a803ddd5d293ae02060ddd730f0bff3dc8a
NTPU <NTPU@503-tr>
2025-05-07 12:20:02 +0800

    j

--- springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -49,7 +49,13 @@ public class ApiController {
 		String result = String.format("Hi %s %d (%s)", 
 								name, age, age >= 18 ? "成年" : "未成年");
 		return result;
-		
 	}
+	
+	/**
+	 * 4. Lab 練習 I
+	 * 路徑: /bmi?h=170&w=60
+	 * 網址: http://localhost:8080/api/bmi?h=170&w=60
+	 * 執行結果: bmi = 20.76
+	 * */
 	
 }
