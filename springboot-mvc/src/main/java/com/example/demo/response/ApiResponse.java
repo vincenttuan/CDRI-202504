ae217c38c639a8cc047cae77f8a008b21a1e0c80
NTPU <NTPU@503-tr>
2025-05-07 14:44:55 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/response/ApiResponse.java
+++ springboot-mvc/src/main/java/com/example/demo/response/ApiResponse.java
@@ -9,17 +9,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse<T> {
-	private Integer status; // 狀態 例如:200, 400
 	private String message; // 訊息 例如: 查詢成功, 新增成功, 計算成功...
 	T data;                 // payload 實際資料
 	
 	// 成功回應
 	public static <T> ApiResponse<T> success(String message, T data) {
-		return new ApiResponse<T>(200, message, data);
+		return new ApiResponse<T>(message, data);
 	}
 	
 	// 失敗回應
-	public static <T> ApiResponse<T> error(int status, String message) {
-		return new ApiResponse<T>(status, message, null);
+	public static <T> ApiResponse<T> error(String message) {
+		return new ApiResponse<T>(message, null);
 	}
 }
