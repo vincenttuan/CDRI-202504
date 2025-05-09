e1de610a775690bece0e23c29f459bde5263abbe
NTPU <NTPU@503-tr>
2025-05-09 11:34:48 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ springboot-mvc/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -13,7 +13,18 @@ public class GlobalExceptionHandler {
 	// 當系統發生例外錯誤
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ApiResponse<Object>> handleException(Exception e) {
-		ApiResponse<Object> apiResponse = ApiResponse.error(e.getMessage());
+		// MethodArgumentTypeMismatchException
+		// NoResourceFoundException
+		String errorMessage = e.toString();
+		switch (e.getClass().getSimpleName()) {
+			case "MethodArgumentTypeMismatchException": 
+				errorMessage = "參數錯誤(" + e.getClass().getSimpleName() + ")";
+				break;
+			case "NoResourceFoundException":
+				errorMessage = "查無網頁(" + e.getClass().getSimpleName() + ")";
+				break;
+		}	
+		ApiResponse<Object> apiResponse = ApiResponse.error(errorMessage);
 		return ResponseEntity.badRequest().body(apiResponse);
 	}
 }
