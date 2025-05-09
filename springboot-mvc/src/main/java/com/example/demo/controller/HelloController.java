e8727e94c5ef43827416d300b87e521abd787aa6
NTPU <NTPU@503-tr>
2025-05-09 09:41:27 +0800

    j

--- springboot-mvc/src/main/java/com/example/demo/controller/HelloController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/HelloController.java
@@ -18,7 +18,6 @@ public class HelloController {
 	}
 	
 	@GetMapping("/hi")
-	@ResponseBody
 	public String hi() {
 		return "Hi " + new Date();
 	}
@@ -26,6 +25,7 @@ public class HelloController {
 	@GetMapping("/welcome")
 	public String welcome(Model model) {
 		// model 裡面放的就是要傳給 jsp 的資料
+		
 		model.addAttribute("name", "阿山哥");
 		model.addAttribute("now", new Date());
 		return "welcome"; // 取 welcome.jsp 檔名的部分
