8c9ca7b0fe1ec3cedafb983373c7df8bf22e9f9d
NTPU <NTPU@503-tr>
2025-05-13 10:18:55 +0800

    ai

--- springboot-ollama/src/main/java/com/example/demo/config/ChatConfig.java
+++ springboot-ollama/src/main/java/com/example/demo/config/ChatConfig.java
@@ -1,5 +1,15 @@
 package com.example.demo.config;
 
-public class ChatConfig {
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class ChatConfig {
+	
+	@Bean // 此物件是被 Spring 管理, 其他程式可以透過 @Autowired 自動綁定來取得該物件(不需要 new)
+	public ChatClient chatClient(ChatClient.Builder builder) {
+		return builder.build();
+	}
+	
 }
