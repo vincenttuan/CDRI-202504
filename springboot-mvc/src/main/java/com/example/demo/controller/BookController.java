fd65f3ea11bf2032f1629fb6ab2f134099004db5
NTPU <NTPU@503-tr>
2025-05-14 12:21:05 +0800

    book

--- springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -74,5 +75,15 @@ public class BookController {
 			return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
 		}
 	}
+	
+	@PatchMapping("/{id}")
+	public ResponseEntity<ApiResponse<Book>> updateBookPrice(@PathVariable Integer id, @RequestBody Book book) {
+		try {
+			bookService.updateBookPrice(id, book.getPrice());
+			return ResponseEntity.ok(ApiResponse.success("修改書籍價格成功", book));
+		} catch (BookException e) {
+			return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
+		}
+	} 
 	
 }
