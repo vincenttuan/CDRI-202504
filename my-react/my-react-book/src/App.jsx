fbc5f654606690c358b01f3f54a2a0ecd8a37acd
NTPU <NTPU@503-tr>
2025-05-14 14:24:25 +0800

    book

--- my-react/my-react-book/src/App.jsx
+++ my-react/my-react-book/src/App.jsx
@@ -4,32 +4,47 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>書籍管理系統(使用 fetch)</h2>
+      <form>
+        id: <input name="id" readOnly /><p />
+        書名: <input name="name" required /><p />
+        價格: <input name="price" required /><p />
+        數量: <input name="amount" required /><p />
+        出刊: <input name="pub" type="checkbox" /><p />
+        <button type="submit">新增</button>
+      </form>
+      <h2>📖 書籍列表</h2>
+      <table border="1" cellPadding="4">
+        <thead>
+          <tr>
+          <th>ID</th>
+          <th>書名</th>
+          <th>價格</th>
+          <th>數量</th>
+          <th>出刊</th>
+          <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <button type='button'>編輯</button>
+              <button type='button'>刪除</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
   )
+  
 }
 
 export default App
