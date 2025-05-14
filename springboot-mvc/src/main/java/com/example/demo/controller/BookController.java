476aa7dd7bc7d234ec1245a7ca402a66a8131e30
NTPU <NTPU@503-tr>
2025-05-14 11:38:59 +0800

    book

--- springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +41,16 @@ public class BookController {
 			return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
 		}
 	}
+	
+	@PostMapping
+	public ResponseEntity<ApiResponse<Book>> addBook(@RequestBody Book book) {
+		try {
+			bookService.addBook(book);
+			return ResponseEntity.ok(ApiResponse.success("新增成功", book));
+		} catch (BookException e) {
+			return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
+		}
+	}
+	
 	
 }
