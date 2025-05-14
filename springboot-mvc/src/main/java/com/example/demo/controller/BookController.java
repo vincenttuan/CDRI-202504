bc79753db636be0b549bde39f091864b80fe0ca0
NTPU <NTPU@503-tr>
2025-05-14 12:05:30 +0800

    book

--- springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.exception.BookException;
@@ -47,6 +49,16 @@ public class BookController {
 		try {
 			bookService.addBook(book);
 			return ResponseEntity.ok(ApiResponse.success("新增成功", book));
+		} catch (BookException e) {
+			return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ApiResponse<String>> deletedBook(@PathVariable Integer id) {
+		try {
+			bookService.deleteBook(id);
+			return ResponseEntity.ok(ApiResponse.success("刪除成功", ""));
 		} catch (BookException e) {
 			return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
 		}
