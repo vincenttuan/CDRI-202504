c595820a51c465703b9c8486cdbe4c687a116ec6
NTPU <NTPU@503-tr>
2025-05-14 11:00:46 +0800

    book

--- springboot-mvc/src/main/java/com/example/demo/service/BookService.java
+++ springboot-mvc/src/main/java/com/example/demo/service/BookService.java
@@ -3,17 +3,18 @@ package com.example.demo.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.example.demo.exception.BookException;
 import com.example.demo.model.Book;
 
 public interface BookService {
 	List<Book> findAllBooks();
 	Optional<Book> getBookById(Integer id);
-	void addBook(Book book);
+	void addBook(Book book) throws BookException;
 	
-	void updateBook(Integer id, Book book);
-	void updateBookName(Integer id, String name);
-	void updateBookPrice(Integer id, Integer price);
-	void updateBookNameAndPrice(Integer id, String name, Integer price);
+	void updateBook(Integer id, Book book) throws BookException;
+	void updateBookName(Integer id, String name) throws BookException;
+	void updateBookPrice(Integer id, Integer price) throws BookException;
+	void updateBookNameAndPrice(Integer id, String name, Integer price) throws BookException;
 	
-	void deleteBook(Integer id);
+	void deleteBook(Integer id) throws BookException;
 }
