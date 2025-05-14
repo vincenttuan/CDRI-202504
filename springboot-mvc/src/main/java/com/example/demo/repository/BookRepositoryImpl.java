a03a2556e7b6ecf644dc19ad12460667569aa3c4
NTPU <NTPU@503-tr>
2025-05-14 10:35:56 +0800

    book

---
+++ springboot-mvc/src/main/java/com/example/demo/repository/BookRepositoryImpl.java
@@ -0,0 +1,49 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Book;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+	// InMemory 版
+	private List<Book> books = new CopyOnWriteArrayList<>();
+	
+	// 初始資料有 4 本書
+	{
+		books.add(new Book(1, "機器貓小叮噹", 12.5, 20, false));
+		books.add(new Book(2, "老夫子", 10.5, 30, false));
+		books.add(new Book(3, "好小子", 8.5, 40, true));
+		books.add(new Book(4, "尼羅河的女兒", 14.5, 50, true));
+	}
+	
+	public List<Book> findAllBooks() {
+		return books;
+	}
+	
+	public Optional<Book> getBookById(Integer id) {
+		return books.stream().filter(book -> book.getId().equals(id)).findFirst();
+	}
+	
+	public boolean addBook(Book book) {
+		return books.add(book);
+	}
+	
+	public boolean updateBook(Integer id, Book book) {
+		return books.set(id, book) != null;
+	}
+	
+	public boolean deleteBook(Integer id) {
+		Optional<Book> optBook = getBookById(id);
+		if(optBook.isPresent()) {
+			return books.remove(optBook.get());
+		}
+		return false;
+	}
+	
+	
+}
