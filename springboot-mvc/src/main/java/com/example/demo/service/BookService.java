4534cd5a204205466effc0ce0006696212c3a122
NTPU <NTPU@503-tr>
2025-05-14 11:00:01 +0800

    j

--- springboot-mvc/src/main/java/com/example/demo/service/BookService.java
+++ springboot-mvc/src/main/java/com/example/demo/service/BookService.java
@@ -1,5 +1,19 @@
 package com.example.demo.service;
 
-public class BookService {
+import java.util.List;
+import java.util.Optional;
 
+import com.example.demo.model.Book;
+
+public interface BookService {
+	List<Book> findAllBooks();
+	Optional<Book> getBookById(Integer id);
+	void addBook(Book book);
+	
+	void updateBook(Integer id, Book book);
+	void updateBookName(Integer id, String name);
+	void updateBookPrice(Integer id, Integer price);
+	void updateBookNameAndPrice(Integer id, String name, Integer price);
+	
+	void deleteBook(Integer id);
 }
