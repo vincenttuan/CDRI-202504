a24fb157787666be1763242d5f315cd894950d1c
NTPU <NTPU@503-tr>
2025-05-16 11:36:36 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/SSRBookController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/SSRBookController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.exception.BookException;
 import com.example.demo.model.Book;
 import com.example.demo.service.BookService;
 
@@ -25,5 +27,18 @@ public class SSRBookController {
 		model.addAttribute("books", books); // 將要傳遞給 jsp 的資料放入 Model 容器中
 		return "book-list"; // 對應到 /WEB-INF/view/book-list.jsp
 	}
+	
+	// 新增書籍
+	@PostMapping("/add")
+	public String addBook(Book book, Model model) {
+		try {
+			bookService.addBook(book);
+		} catch (BookException e) {
+			model.addAttribute("message", "新增錯誤: " + e.getMessage());
+			return "error.jsp";
+		}
+		return "redirect: /ssr/book";
+	}
+	
 	
 }
--- springboot-mvc/src/main/webapp/WEB-INF/view/book-list.jsp
+++ springboot-mvc/src/main/webapp/WEB-INF/view/book-list.jsp
@@ -10,10 +10,10 @@
 		<%@ include file="include/menu.jsp" %>
 		<div>
 			<form method="post" action="/ssr/book/add">
-				書名: <input type="text" name="name" required /><p />
-				價格: <input type="number" name="price" step="0.1" required /><p />
-				數量: <input type="number" name="amount" required /><p />
-				出刊: <input type="checkbox" name="pub" required /><p />
+				書名: <input type="text" name="name" required /><br />
+				價格: <input type="number" name="price" step="0.1" required /><br />
+				數量: <input type="number" name="amount" required /><br />
+				出刊: <input type="checkbox" name="pub" required /><br />
 				<button type="submit">送出</button>
 			</form>
 		</div>
