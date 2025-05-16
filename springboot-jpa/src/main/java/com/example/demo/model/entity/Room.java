0cde42c5cb5f2a7c92870648bb21584996708028
NTPU <NTPU@503-tr>
2025-05-16 16:17:38 +0800

    j

---
+++ springboot-jpa/src/main/java/com/example/demo/model/entity/Room.java
@@ -0,0 +1,15 @@
+package com.example.demo.model.entity;
+
+/**
+Table name: room
++---------+-----------+-----------+
+| room_id | room_name | room_size |
++---------+-----------+-----------+
+|  101    |  101(L)   |   100     |  
++---------+-----------+-----------+
+*/
+
+
+public class Room {
+
+}
