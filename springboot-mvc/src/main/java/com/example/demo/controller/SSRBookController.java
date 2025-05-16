b909ab97bb4e3559ba71f17a12f8f19e620ffeda
NTPU <NTPU@503-tr>
2025-05-16 14:15:31 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/SSRBookController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/SSRBookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,7 +46,8 @@ public class SSRBookController {
 	}
 	
 	// 刪除書籍
-	@GetMapping("/delete/{id}")
+	//@GetMapping("/delete/{id}")
+	@DeleteMapping("/delete/{id}")
 	public String deleteBook(@PathVariable Integer id, Model model) {
 		try {
 			bookService.deleteBook(id);
