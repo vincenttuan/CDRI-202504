ca8e9e4d9d47e4342cc8a9dcbcdbd70bb5f076ec
NTPU <NTPU@503-tr>
2025-05-19 14:37:36 +0800

    jpa

--- springboot-jpa/src/main/java/com/example/demo/model/dto/RoomDto.java
+++ springboot-jpa/src/main/java/com/example/demo/model/dto/RoomDto.java
@@ -12,15 +12,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RoomDto {
-	@NotNull(message = "房號不可以是空值")
-	@Range(min = 1, max = 9999, message = "房號必須介於 {min} ~ {max} 之間")
+	@NotNull(message = "{roomDto.roomId.notNull}")
+	@Range(min = 1, max = 9999, message = "{roomDto.roomId.range}")
 	private Integer roomId;
 	
-	@NotNull(message = "房名不可以是空值")
-	@Size(min = 2, message = "房名至少要有 {min} 個字")
+	@NotNull(message = "{roomDto.roomName.notNull}")
+	@Size(min = 2, message = "{roomDto.roomName.size}")
 	private String roomName;
 	
-	@NotNull(message = "房間人數不可以是空值")
-	@Range(min = 1, max = 500, message = "房間人數必須介於 {min} ~ {max} 之間")
+	@NotNull(message = "{roomDto.roomSize.notNull}")
+	@Range(min = 1, max = 500, message = "{roomDto.roomSize.range}")
 	private Integer roomSize;
 }
