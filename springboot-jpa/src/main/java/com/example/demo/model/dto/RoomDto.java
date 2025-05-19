e8d12f1f1dec3ca7e62dd5290a092a9802c50b36
NTPU <NTPU@503-tr>
2025-05-19 14:16:01 +0800

    jpa

--- springboot-jpa/src/main/java/com/example/demo/model/dto/RoomDto.java
+++ springboot-jpa/src/main/java/com/example/demo/model/dto/RoomDto.java
@@ -3,6 +3,7 @@ package com.example.demo.model.dto;
 import org.hibernate.validator.constraints.Range;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RoomDto {
+	@NotNull(message = "房號不可以是空值")
+	@Range(min = 1, max = 9999, message = "房號必須介於 {min} ~ {max} 之間")
 	private Integer roomId;
 	
+	@NotNull(message = "房名不可以是空值")
+	@Size(min = 2, message = "房名至少要有 {min} 個字")
 	private String roomName;
 	
 	@NotNull(message = "房間人數不可以是空值")
