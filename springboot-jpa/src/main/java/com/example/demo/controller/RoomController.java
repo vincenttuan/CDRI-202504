4f227a09a0cb597d097ac3e1cc3eb27c774af70c
NTPU <NTPU@503-tr>
2025-05-19 15:36:57 +0800

    jpa

--- springboot-jpa/src/main/java/com/example/demo/controller/RoomController.java
+++ springboot-jpa/src/main/java/com/example/demo/controller/RoomController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -77,6 +79,18 @@ public class RoomController {
 		// 進行修改
 		roomService.updateRoom(roomId, roomDto);
 		return "redirect:/rooms";
+	}
+	
+	@DeleteMapping("/delete/{roomId}")
+	public String deleteRoom(@PathVariable Integer roomId) {
+		roomService.deleteRoom(roomId);
+		return "redirect:/rooms"; // 重導到 /rooms 頁面
+	}
+	
+	@ExceptionHandler({Exception.class})
+	public String handleException(Exception e, Model model) {
+		model.addAttribute("message", e.getMessage());
+		return "error";
 	}
 	
 }
