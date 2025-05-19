f5f953b5c905bc8c53b8427e3b087c5d4313ed92
NTPU <NTPU@503-tr>
2025-05-19 10:01:21 +0800

    jpa

--- springboot-jpa/src/main/java/com/example/demo/repository/RoomRepository.java
+++ springboot-jpa/src/main/java/com/example/demo/repository/RoomRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,8 @@ import com.example.demo.model.entity.Room;
 // Spring JPA
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> { // Room: entity, Integer: @Id 的型別
-
+	// 預設會實現 CRUD
+	// 自定義查詢
+	// 1. 查詢 roomSize 大於指定值得房間(自動生成 SQL)
+	List<Room> findByRoomSizeGreaterThan(Integer size);
 }
