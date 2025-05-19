934c0a86d592ab21bf728cd86c35a9a468a1009e
NTPU <NTPU@503-tr>
2025-05-19 11:02:35 +0800

    jpa

--- springboot-jpa/src/main/java/com/example/demo/service/impl/RoomServiceImpl.java
+++ springboot-jpa/src/main/java/com/example/demo/service/impl/RoomServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.mapper.RoomMapper;
 import com.example.demo.model.dto.RoomDto;
+import com.example.demo.model.entity.Room;
 import com.example.demo.repository.RoomRepository;
 import com.example.demo.service.RoomService;
 
@@ -15,15 +17,22 @@ public class RoomServiceImpl implements RoomService {
 	@Autowired
 	private RoomRepository roomRepository;
 	
+	@Autowired
+	private RoomMapper roomMapper;
+	
 	@Override
 	public List<RoomDto> findAllRooms() {
-		return null;
+		return roomRepository.findAll()   // room 集合
+							 .stream()    // room 串流
+							 .map(roomMapper::toDto) // roomDto 串流  .map(room -> roomMapper.toDto(room))
+							 .toList();   // roomDto 集合
 	}
 
 	@Override
 	public RoomDto getRoomById(Integer roomId) {
-		// TODO Auto-generated method stub
-		return null;
+		Room room = roomRepository.findById(roomId)
+				.orElseThrow(() -> new RoomNotFoundException("找不到會議室: roodId=" + roomId));
+		return roomMapper.toDto(room);
 	}
 
 	@Override
