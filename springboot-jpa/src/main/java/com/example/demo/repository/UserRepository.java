bc7fe99337876246eb14018a5311b32c4013ef64
NTPU <NTPU@503-tr>
2025-05-21 10:31:45 +0800

    j

--- springboot-jpa/src/main/java/com/example/demo/repository/UserRepository.java
+++ springboot-jpa/src/main/java/com/example/demo/repository/UserRepository.java
@@ -10,4 +10,6 @@ import com.example.demo.model.entity.User;
 public interface UserRepository extends JpaRepository<User, Integer> {
 	@Query(value = "select user_id, username, password_hash, salt, email, active, role from users where username=:username", nativeQuery = true)
 	User getUser(String username);
+	
+	// 可以自行練習其他寫法...
 }
