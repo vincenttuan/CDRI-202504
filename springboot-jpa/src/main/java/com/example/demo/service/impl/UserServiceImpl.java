5ebae129aa05cf0cee20b8b9d0c9db100e67cd4c
NTPU <NTPU@503-tr>
2025-05-21 10:55:15 +0800

    j

--- springboot-jpa/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ springboot-jpa/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.demo.mapper.UserMapper;
 import com.example.demo.model.dto.UserDto;
+import com.example.demo.model.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
@@ -11,9 +13,16 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private UserMapper userMapper;
+	
 	@Override
 	public UserDto getUser(String username) {
-		return null;
+		User user = userRepository.getUser(username);
+		if(user == null) {
+			return null;
+		}
+		return userMapper.toDto(user);
 	}
 
 	@Override
