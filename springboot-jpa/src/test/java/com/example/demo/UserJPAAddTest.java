8068dee2508f108865a0f45ab5d08d1c35058301
NTPU <NTPU@503-tr>
2025-05-21 11:04:07 +0800

    j

--- springboot-jpa/src/test/java/com/example/demo/UserJPAAddTest.java
+++ springboot-jpa/src/test/java/com/example/demo/UserJPAAddTest.java
@@ -1,14 +1,9 @@
 package com.example.demo;
 
-import java.util.Scanner;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.demo.model.entity.Room;
-import com.example.demo.repository.RoomRepository;
-import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
 @SpringBootTest
