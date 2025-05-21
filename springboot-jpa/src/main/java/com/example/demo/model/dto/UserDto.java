5e365fdbd78ab780fdda513ad4942bacf995acbb
NTPU <NTPU@503-tr>
2025-05-21 11:08:53 +0800

    j

--- springboot-jpa/src/main/java/com/example/demo/model/dto/UserDto.java
+++ springboot-jpa/src/main/java/com/example/demo/model/dto/UserDto.java
@@ -1,7 +1,11 @@
 package com.example.demo.model.dto;
 
+import lombok.Data;
+import lombok.ToString;
+
 // UserDto 對應於 User Entity
 // 屬性名稱可以與所對應的 entity 不同
+@Data
 public class UserDto {
 	private Integer userId; // 使用者 Id
 	private String username; // 使用者名稱
