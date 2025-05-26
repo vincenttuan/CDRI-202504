60e36354e44c5ff0ab7fdf253d40406ac30774a1
NTPU <NTPU@503-tr>
2025-05-26 10:05:18 +0800

    j

---
+++ springboot-jpa-book/src/main/java/com/example/demo/SpringbootJpaBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaBookApplication.class, args);
+	}
+
+}
