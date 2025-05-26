bf524275b85f1a8c091d30ea1717898a215bf94a
NTPU <NTPU@503-tr>
2025-05-26 15:43:01 +0800

    j

--- springboot-jpa-book/src/test/java/com/example/demo/Test_RemoveBookFromPublisher.java
+++ springboot-jpa-book/src/test/java/com/example/demo/Test_RemoveBookFromPublisher.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
 import com.example.demo.model.entity.Book;
 import com.example.demo.model.entity.Publisher;
@@ -22,10 +23,11 @@ public class Test_RemoveBookFromPublisher {
 	
 	@Test
 	@Transactional
+	@Commit // 在測試環境中使用 @Transactional 要配合使用 @Commit 讓事務提交
 	public void remove() {
 		
 		Book book1 = bookRepository.findById(1).get();
-		Publisher publisher = publisherRepository.findById(1).get();
+		Publisher publisher = publisherRepository.findById(5).get();
 		publisherRepository.deleteBookFromPublisher(publisher.getId(), book1.getId());
 		
 	}
