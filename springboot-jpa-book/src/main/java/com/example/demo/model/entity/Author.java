f4e2907078ec8089e87fc44f2e1828564af51355
NTPU <NTPU@503-tr>
2025-05-26 10:13:23 +0800

    j

--- springboot-jpa-book/src/main/java/com/example/demo/model/entity/Author.java
+++ springboot-jpa-book/src/main/java/com/example/demo/model/entity/Author.java
@@ -1,5 +1,25 @@
 package com.example.demo.model.entity;
 
-public class Author {
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Author {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 50, nullable = false)
+	private String name;
+	
+	private List<Book> books;
+	
 }
--- springboot-jpa-book/src/main/java/com/example/demo/model/entity/Book.java
+++ springboot-jpa-book/src/main/java/com/example/demo/model/entity/Book.java
@@ -1,5 +1,23 @@
 package com.example.demo.model.entity;
 
-public class Book {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 100, nullable = false)
+	private String name;
+	
+	private Author author;
+	
 }
