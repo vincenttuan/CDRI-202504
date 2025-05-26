c9695276b1fa9ec6dd1ac85f4973c3c90196c0e3
NTPU <NTPU@503-tr>
2025-05-26 12:00:56 +0800

    j

--- springboot-jpa-book/src/main/java/com/example/demo/model/entity/Book.java
+++ springboot-jpa-book/src/main/java/com/example/demo/model/entity/Book.java
@@ -2,6 +2,7 @@ package com.example.demo.model.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,7 +21,7 @@ public class Book {
 	@Column(length = 100, nullable = false)
 	private String name;
 	
-	@ManyToOne
+	@ManyToOne()
 	@JoinColumn(name = "author_id")
 	private Author author;
 	
---
+++ springboot-jpa-book/src/test/java/com/example/demo/Test_ReadBook.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.entity.Book;
+import com.example.demo.repository.BookRepository;
+
+@SpringBootTest
+public class Test_ReadBook {
+	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@Test
+	public void read() {
+		// 查詢書籍
+		List<Book> books = bookRepository.findAll();
+		books.forEach(book -> {
+			System.out.printf("序號:%d 書名:%s%n", book.getId(), book.getName());
+		});
+		
+		// 查詢書籍 + 作者
+		books.forEach(book -> {
+			System.out.printf("序號:%d 書名:%s 作者:%s%n", book.getId(), book.getName(), book.getAuthor().getName());
+		});
+		
+		
+	}
+	
+}
