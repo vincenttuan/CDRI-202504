0de0cb1c5eacac5ba550439396d9e577f3ce3269
NTPU <NTPU@503-tr>
2025-05-26 14:11:48 +0800

    j

--- springboot-jpa-book/src/test/java/com/example/demo/Test_ReadAuthor.java
+++ springboot-jpa-book/src/test/java/com/example/demo/Test_ReadAuthor.java
@@ -18,17 +18,19 @@ public class Test_ReadAuthor {
 	@Test
 	//@Transactional
 	public void read() {
-		// 查詢作者
+		// 查詢作者(含自傳)
 		List<Author> authors = authorRepository.findAll();
 		authors.forEach(author -> {
-			System.out.printf("序號:%d 姓名:%s%n", author.getId(), author.getName());
+			System.out.printf("序號:%d 姓名:%s 自傳:%s%n", 
+					author.getId(), author.getName(), author.getBiography().getDetails());
 			
 		});
 		
-		// 查詢作者+書籍
+		// 查詢作者(含自傳)+書籍
 		List<Author> authors2 = authorRepository.findAllWithBooks();
 		authors2.forEach(author -> {
-			System.out.printf("序號:%d 姓名:%s 著作數量:%d%n", author.getId(), author.getName(), author.getBooks().size());
+			System.out.printf("序號:%d 姓名:%s 自傳:%s 著作數量:%d%n", 
+					author.getId(), author.getName(), author.getBiography().getDetails(), author.getBooks().size());
 			
 		});
 		
