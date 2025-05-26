3217cf387049621e67de98497c4c263f776f6746
NTPU <NTPU@503-tr>
2025-05-26 13:57:56 +0800

    java

--- springboot-jpa-book/src/main/java/com/example/demo/model/entity/Biography.java
+++ springboot-jpa-book/src/main/java/com/example/demo/model/entity/Biography.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 
@@ -20,5 +21,6 @@ public class Biography {
 	private String details;
 	
 	@OneToOne
+	@JoinColumn(name = "author_id") // 外鍵
 	private Author author;
 }
