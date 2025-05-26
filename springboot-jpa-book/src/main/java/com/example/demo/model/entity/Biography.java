c696e630f82ddb418b0ad1ea44286fbd3af8f915
NTPU <NTPU@503-tr>
2025-05-26 13:53:29 +0800

    java

--- springboot-jpa-book/src/main/java/com/example/demo/model/entity/Biography.java
+++ springboot-jpa-book/src/main/java/com/example/demo/model/entity/Biography.java
@@ -1,33 +1,24 @@
 package com.example.demo.model.entity;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Data
 @Entity
-public class Author {
+public class Biography {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-	@Column(length = 50, nullable = false)
-	private String name;
+	@Column(length = 255)
+	private String details;
 	
 	@OneToOne
-	private Biography biography;
-	
-	@OneToMany(mappedBy = "author")
-	//@OneToMany(mappedBy = "author", fetch = FetchType.EAGER)
-	private List<Book> books;
-	
+	private Author author;
 }
