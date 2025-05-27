57ac3cd8f8d8cc154bc6d50e9e28e0a702b07867
NTPU <NTPU@503-tr>
2025-05-27 10:17:10 +0800

    j

--- springboot-tx/src/main/java/com/example/demo/model/entity/Stock.java
+++ springboot-tx/src/main/java/com/example/demo/model/entity/Stock.java
@@ -1,6 +1,18 @@
 package com.example.demo.model.entity;
 
-// 庫存
-public class Stock {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+// 庫存
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Stock {
+	@Id
+	private Integer bookId; // 與書本的 book.bookId 對應
+	private Integer bookAmount; // 庫存數量
 }
