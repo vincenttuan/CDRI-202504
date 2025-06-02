477235e53f8819edf4475c0cff57db566f1ae4c8
NTPU <NTPU@503-tr>
2025-06-02 10:29:18 +0800

    ws

---
+++ springboot-websocket/src/main/java/com/example/demo/ChatController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+	@MessageMapping("/chat")
+	@SendTo("/topic/messages")
+	public ChatMessage send(ChatMessage message) {
+		return message;
+	}
+}
