2f316195891d848f0249ff7bed8b61d7b8f0a9bb
NTPU <NTPU@503-tr>
2025-06-03 12:01:51 +0800

    j

---
+++ springboot-websocket-bingo/src/main/java/com/example/demo/SpringbootWebsocketBingoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebsocketBingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebsocketBingoApplication.class, args);
+	}
+
+}
