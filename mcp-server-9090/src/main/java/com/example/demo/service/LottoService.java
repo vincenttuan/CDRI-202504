93fce2a85202b058f4c8a3363546548d42da608f
NTPU <NTPU@503-tr>
2025-06-04 11:43:09 +0800

    web

--- mcp-server-9090/src/main/java/com/example/demo/service/LottoService.java
+++ mcp-server-9090/src/main/java/com/example/demo/service/LottoService.java
@@ -2,15 +2,18 @@ package com.example.demo.service;
 
 import java.util.Random;
 
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LottoService {
 	
-	/** 取得樂透號碼
+	/** 得到樂透電腦選號
 	 * @param amount 購買數量
 	 */
-	public String lotto(Integer amount) {
+	@Tool(name = "lotto", description = "得到樂透電腦選號")
+	public String lotto(@ToolParam(description = "樂透電腦選號購買的數量") Integer amount) {
 		Random random = new Random();
 		return "LottoService 產生樂透號碼:" + random.nextInt(10); // 0~9
 	}
@@ -18,7 +21,8 @@ public class LottoService {
 	/** 樂透結帳
 	 * @param amount 購買數量
 	 */
-	public String checkout(Integer amount) {
+	@Tool(name = "checkout", description = "樂透結帳/付款")
+	public String checkout(@ToolParam(description = "要結帳的數量") Integer amount) {
 		return "LottoService 樂透結帳:$" + (amount * 50);
 	}
 }
