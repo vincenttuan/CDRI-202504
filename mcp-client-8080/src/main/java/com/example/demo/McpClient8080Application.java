aa50419775ba1e2e3d7d6983a6a205c48149a260
NTPU <NTPU@503-tr>
2025-06-04 11:55:08 +0800

    j

--- mcp-client-8080/src/main/java/com/example/demo/McpClient8080Application.java
+++ mcp-client-8080/src/main/java/com/example/demo/McpClient8080Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class McpClient8080Application {
@@ -9,5 +13,17 @@ public class McpClient8080Application {
 	public static void main(String[] args) {
 		SpringApplication.run(McpClient8080Application.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner runner(ChatClient.Builder chatClientBuilder, SyncMcpToolCallbackProvider toolCallbackProvider) {
+		return args -> {
+			// 建立 ChatClient 並將 callback provider 註冊進去
+			ChatClient chatClient = chatClientBuilder.defaultTools(toolCallbackProvider).build();
+			
+			String prompt1 = "今天似乎運氣不錯想買5張樂透彩券";
+			String response1 = chatClient.prompt().user(prompt1).call().content();
+			
+			System.out.println("AI Agent 回應:" + response1);
+		};
+	}
 }
