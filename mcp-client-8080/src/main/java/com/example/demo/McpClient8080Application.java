dfc7518c51a8b4ea745300d6f57aafb53d0bc25d
NTPU <NTPU@503-tr>
2025-06-04 12:21:34 +0800

    web

--- mcp-client-8080/src/main/java/com/example/demo/McpClient8080Application.java
+++ mcp-client-8080/src/main/java/com/example/demo/McpClient8080Application.java
@@ -20,8 +20,8 @@ public class McpClient8080Application {
 			// 建立 ChatClient 並將 callback provider 註冊進去
 			ChatClient chatClient = chatClientBuilder.defaultTools(toolCallbackProvider).build();
 			
-			//String prompt1 = "今天似乎運氣不錯想買5張樂透彩券並顯示這5張號碼, 並幫我結帳";
-			String prompt1 = "今天似乎運氣不錯想買5組樂透彩券並顯示這5組號碼, 不要幫我結帳";
+			String prompt1 = "今天似乎運氣不錯想買5組樂透彩券並顯示這5組號碼, 並幫我結帳";
+			//String prompt1 = "今天似乎運氣不錯想買5組樂透彩券並顯示這5組號碼, 不要幫我結帳";
 			System.out.println(prompt1);
 			
 			String response1 = chatClient.prompt().user(prompt1).call().content();
