34a0ac7c642ebaed0b427277fabd99ce83a22093
NTPU <NTPU@503-tr>
2025-06-05 09:58:59 +0800

    j

---
+++ mcp-server-9091/src/main/java/com/example/demo/McpServer9091Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServer9091Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpServer9091Application.class, args);
+	}
+
+}
