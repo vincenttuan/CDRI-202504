a6957f61695473c8b8509e9e8a9e0fe61bf796a3
NTPU <NTPU@503-tr>
2025-06-09 11:12:42 +0800

    j

--- springboot-aop/src/main/java/com/example/demo/proxy/CalcTest.java
+++ springboot-aop/src/main/java/com/example/demo/proxy/CalcTest.java
@@ -5,9 +5,13 @@ public class CalcTest {
 	public static void main(String[] args) {
 		Calc calc = new CalcImpl();
 		Calc calcProxy = new CalcProxy(calc);
+		// 加法
 		System.out.println(calcProxy.add(10, 20));
 		System.out.println(calcProxy.add(null, 20));
-		
+		// 除法
+		System.out.println(calcProxy.div(20, 10)); // 正常
+		System.out.println(calcProxy.div(null, 10));
+		System.out.println(calcProxy.div(20, 0));
 	}
 
 }
