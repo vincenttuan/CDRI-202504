1b1b34d7465b26392eabc209f01a634298a66750
NTPU <NTPU@503-tr>
2025-06-09 10:06:07 +0800

    j

--- springboot-aop/src/main/java/com/example/demo/proxy/PersonTest.java
+++ springboot-aop/src/main/java/com/example/demo/proxy/PersonTest.java
@@ -4,8 +4,14 @@ public class PersonTest {
 
 	public static void main(String[] args) {
 		// 一般用法
-		Person man = new Man();
-		Person woman = new Woman();
+		//Person man = new Man();
+		//Person woman = new Woman();
+		//man.work();
+		//woman.work();
+		
+		// 使用代理
+		Person man = new PersonProxy(new Man());
+		Person woman = new PersonProxy(new Woman());
 		man.work();
 		woman.work();
 	}
