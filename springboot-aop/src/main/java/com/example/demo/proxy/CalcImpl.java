56aa0104f94833e33f0e3075791a1a12b78f9ae6
NTPU <NTPU@503-tr>
2025-06-09 11:01:30 +0800

    j

---
+++ springboot-aop/src/main/java/com/example/demo/proxy/CalcImpl.java
@@ -0,0 +1,17 @@
+package com.example.demo.proxy;
+
+public class CalcImpl implements Calc {
+
+	@Override
+	public Integer add(Integer x, Integer y) {
+		// 業務邏輯
+		return x + y;
+	}
+
+	@Override
+	public Integer div(Integer x, Integer y) {
+		// 業務邏輯
+		return x / y;
+	}
+	
+}
