22037cf367780f0c2d21ad4838d2f9aa9acb1fd3
NTPU <NTPU@503-tr>
2025-06-09 09:51:56 +0800

    j

--- springboot-aop/src/main/java/com/example/demo/proxy/Man.java
+++ springboot-aop/src/main/java/com/example/demo/proxy/Man.java
@@ -4,6 +4,7 @@ public class Man implements Person {
 
 	@Override
 	public void work() {
+		// 業務邏輯
 		System.out.println("Man 上班工作");
 	}
 
--- springboot-aop/src/main/java/com/example/demo/proxy/Woman.java
+++ springboot-aop/src/main/java/com/example/demo/proxy/Woman.java
@@ -4,6 +4,7 @@ public class Woman implements Person {
 
 	@Override
 	public void work() {
+		// 業務邏輯
 		System.out.println("Woman 市場買菜");
 	}
 	
