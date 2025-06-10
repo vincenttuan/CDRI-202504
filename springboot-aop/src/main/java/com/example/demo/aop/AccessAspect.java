53f775ae7fbf350a8cdf9337bf9e85a20bb16213
NTPU <NTPU@503-tr>
2025-06-10 11:42:51 +0800

    j

--- springboot-aop/src/main/java/com/example/demo/aop/AccessAspect.java
+++ springboot-aop/src/main/java/com/example/demo/aop/AccessAspect.java
@@ -3,6 +3,7 @@ package com.example.demo.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -15,7 +16,7 @@ public class AccessAspect {
 	@Autowired
 	private AccessController accessController;
 	
-	@Around(value = "* com.example.demo.aop.FloorServiceImpl.enterFloor(..)")
+	@Around(value = "execution(* com.example.demo.aop.FloorServiceImpl.enterFloor(..))")
 	public Object checkAccess(ProceedingJoinPoint joinPoint) throws Throwable {
 		Object result = null;
 		
