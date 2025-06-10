9b72acbb8157d1134d1538d24b8bd2e45cc3c6db
NTPU <NTPU@503-tr>
2025-06-10 09:50:53 +0800

    j

--- springboot-aop/src/main/java/com/example/demo/proxy/CalcImpl.java
+++ springboot-aop/src/main/java/com/example/demo/proxy/CalcImpl.java
@@ -1,5 +1,8 @@
 package com.example.demo.proxy;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CalcImpl implements Calc {
 
 	@Override
---
+++ springboot-aop/src/test/java/com/example/demo/AOPTest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.proxy.Calc;
+
+@SpringBootTest
+public class AOPTest {
+	
+	@Autowired
+	private Calc calc;
+	
+	@Test
+	public void test() {
+		Integer result1 = calc.add(20,  10);
+		Integer result2 = calc.div(20,  10);
+		
+		System.out.println(result1);
+		System.out.println(result2);
+	}
+	
+}
