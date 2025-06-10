3aaaba649b4ef6be8783d6540a756fcc481c84ca
NTPU <NTPU@503-tr>
2025-06-10 12:08:30 +0800

    j

---
+++ springboot-aop/src/test/java/com/example/demo/AOPCartTest.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.aop.CartService;
+import com.example.demo.aop.FloorService;
+
+@SpringBootTest
+public class AOPCartTest {
+	
+	@Autowired
+	private CartService cartService;
+	
+	@Test
+	public void test() {
+		String productId = "A01";
+		Integer quantity = 10;
+		Double price = 15.5;
+		
+		cartService.addToCart(productId, quantity, price);
+	}
+	
+}
