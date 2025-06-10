91c6d487697edee95363b1dcdf353a2fffdab952
NTPU <NTPU@503-tr>
2025-06-10 11:58:24 +0800

    j

---
+++ springboot-aop/src/main/java/com/example/demo/aop/CartService.java
@@ -0,0 +1,10 @@
+package com.example.demo.aop;
+
+// 驗證商品 ID 不可以 null
+// 商品數量介於 1~100 間
+// 商品價格 > 0
+public interface CartService {
+	
+	void addToCart(String productId, int quantity, double price);
+	
+}
