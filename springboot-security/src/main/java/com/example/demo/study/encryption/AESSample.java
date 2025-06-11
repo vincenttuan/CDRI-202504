3f555b37db3ca9da2c25f69d4344ea10af311cce
NTPU <NTPU@503-tr>
2025-06-11 10:30:45 +0800

    j

--- springboot-security/src/main/java/com/example/demo/study/encryption/AESSample.java
+++ springboot-security/src/main/java/com/example/demo/study/encryption/AESSample.java
@@ -1,6 +1,7 @@
 package com.example.demo.study.encryption;
 
 import java.util.Arrays;
+import java.util.Base64;
 
 import javax.crypto.spec.SecretKeySpec;
 
@@ -18,7 +19,7 @@ public class AESSample {
 		System.out.println("---------------------------");
 		
 		// 利用 AES 進行加密
-		// 加密流程: 明文 -> 加密(encryptedECB)
+		// 加密流程: 明文 -> 加密(encryptedECB) -> 進行 Base64 編碼(以利儲存)
 		// 解密流程: 解密(decryptedECB) -> 明文
 		
 		// 1. 建立 AES 密鑰規範
@@ -27,6 +28,9 @@ public class AESSample {
 		byte[] encryptedECB = KeyUtil.encryptWithAESKey(aesKeySpec, originalText);
 		// 3. 印出加密後的資訊
 		System.out.printf("加密後:%s%n", Arrays.toString(encryptedECB));
+		// 4. 將 byte[] 轉 Base64(編碼)
+		String encodingECBBase64 = Base64.getEncoder().encodeToString(encryptedECB);
+		System.out.printf("加密後(Base64):%s%n", encodingECBBase64);
 		
 		
 		
