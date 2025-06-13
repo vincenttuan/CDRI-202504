aa103e9f4de9570480dc1aec6119e1ea9e024abc
NTPU <NTPU@503-tr>
2025-06-13 10:05:04 +0800

    j

--- springboot-security/src/main/java/com/example/demo/study/mac/MACExample.java
+++ springboot-security/src/main/java/com/example/demo/study/mac/MACExample.java
@@ -24,6 +24,21 @@ public class MACExample {
 		String macHexValue = KeyUtil.bytesToHex(macValue);
 		System.out.printf("MAC(Hex): %s%n", macHexValue);
 		
+		// --------------------------------------------------------------------
+		// 5. 在實際應用中, 接收方會收到 message 與 macHexValue
+		//    此時 message 要與 macKey(雙方都要有相同的密鑰) 產生出 computedMacHexValue 值
+		//    並與 macHexValue 進行比對
+		String receivedMessage = message; // 收到訊息(但尚未確定來源)
+		byte[] computedMacValue = KeyUtil.generateMac(macKey, receivedMessage);
+		String computedMacHexValue = KeyUtil.bytesToHex(computedMacValue);
+		
+		// 6. 比較 macHexValue 與 computedMacHexValue 是否相同 ?
+		if(macHexValue.equals(computedMacHexValue)) {
+			System.out.println("MAC 驗證成功, 資料來源正確: " + receivedMessage);
+		} else {
+			System.out.println("MAC 驗證失敗, 資料來源錯誤");
+		}
+		
 	}
 	
 }
