a0fee09c7d12dbba48fd4f414372baa8b50b865c
NTPU <NTPU@503-tr>
2025-06-16 10:20:40 +0800

    j

--- springboot-security/src/main/java/com/example/demo/study/random/TOTP.java
+++ springboot-security/src/main/java/com/example/demo/study/random/TOTP.java
@@ -1,5 +1,9 @@
 package com.example.demo.study.random;
 
+import java.util.Base64;
+
+import com.example.demo.study.security.KeyUtil;
+
 /*
  * 
  * TOTP (Time-based One-Time Password) 是一種一次性密碼（OTP）算法，它的特點是根據當前的時間值生成。
@@ -12,5 +16,19 @@ package com.example.demo.study.random;
  * TOTP: 是 OTP 的一種，其特點是根據當前時間生成密碼。
 */
 public class TOTP {
-
+	
+	public static void main(String[] args) throws Exception {
+		
+	}
+	
+	public static String generateTOTP(String username) throws Exception {
+		// 金鑰(以登入者的名稱當作金鑰)
+		String secret = Base64.getEncoder().encodeToString(username.getBytes());
+		// 現在秒數 30 的倍數就會更新
+		long timeInterval = System.currentTimeMillis() / 1000L / 30L;
+		// 得到 TOTP 密碼
+		String totp = KeyUtil.generateTOTP(secret, timeInterval, "HMACSHA256");
+		return totp;
+	}
+	
 }
