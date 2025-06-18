97175b122bdfeb8244b6da81d83159c1d8dc3c2e
NTPU <NTPU@503-tr>
2025-06-18 10:36:25 +0800

    j

--- springboot-springsecurity/src/main/java/com/example/demo/config/SecurityConfig.java
+++ springboot-springsecurity/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +29,29 @@ public class SecurityConfig {
 		
 		return http.build();
 	}
+	
+	@Bean
+	public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+		// 建立使用者-InMemory版本
+		UserDetails user = User.builder()
+				.username("user")
+				.password(passwordEncoder.encode("1234"))
+				.roles("USER")
+				.build();
+		
+		UserDetails admin = User.builder()
+				.username("admin")
+				.password(passwordEncoder.encode("5678"))
+				.roles("USER", "ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); // 建立加密元件(已加鹽)
+	}
+	
 	
 }
